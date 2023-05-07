249c629b5ae0197ea33defcd082e4ae3b98409bd
Mohamed Alahmady <mohamedalahmady614@gmail.com>
2023-05-07 15:55:33 +0300

    update .m file

--- Modulation_Schemes_file.m
+++ Modulation_Schemes_file.m
@@ -235,7 +235,7 @@ hold off
 clear
 clc
 close all
-number_of_enamples=5000;                          % total number of Realizations
+number_of_enamples=500;                          % total number of Realizations
 number_of_bits=100;                              % number of bits in each Realization
 Tb=7;                                            % number of samples for each bit
 number_of_samples=number_of_bits*Tb;             % total number of samples for each Realization
@@ -254,7 +254,7 @@ for i = 1 : number_of_samples/2
     average(1,350+i) = mean(((Total_Transmitted_Data(:,350))).*conj((Total_Transmitted_Data(:,350+i))));    
 end
 figure
-plot(-(number_of_samples/2-1):1:number_of_samples/2, (average))
+plot(-(number_of_samples/2-1):1:number_of_samples/2, abs(average))
 grid on
 xlabel("\tau")
 ylabel("ACF")
